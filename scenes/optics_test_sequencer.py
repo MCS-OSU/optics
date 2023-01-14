f8437266d843f44dd58224bcde4be56b8b671a72
Mazen Alotaibi <alotaima@oregonstat.edu>
2023-01-14 07:49:53 +0000

    remove redundant has_more_scenes()

--- scenes/optics_test_sequencer.py
+++ scenes/optics_test_sequencer.py
@@ -29,7 +29,7 @@ class OpticsTestSequencer():
         while True:
             #print('...scanning for job requests')
             self.answer_job_requests()
-            time.sleep(3)
+            time.sleep(10)
 
  
     def answer_job_requests(self):
@@ -45,8 +45,34 @@ class OpticsTestSequencer():
             optics_info(f' machine: {machine} command: {command}')
             types_to_skip = self.optics_spec.types_to_skip
             if command == JOB_REQUEST:
+                optics_info(f'job request from {machine} for {self.optics_spec.name}')
+                next_scene_path = self.test_register.assign_next_scene(self.scene_path_list, types_to_skip)
+                if NO_MORE_SCENES_TO_RUN == next_scene_path:
+                    add_last_line(session, get_register_control_message(NO_MORE_SCENES_TO_RUN, '---'))
+                    optics_info('NO MORE SCENES TO RUN...')
+                else:
+                    optics_info(f'assigning {next_scene_path}')
+                    add_last_line(session, get_register_control_message(JOB_ASSIGN, next_scene_path))                    
+            else:
+                optics_error(f'unknown command {command} in {session}')
+
+
+    def answer_job_requests_orig(self):
+        sessions = self.runner_sessions.get_sessions_with_job_requests()
+        optics_debug(f'sessions with job_requests {len(sessions)}')
+        for session in sessions:
+            optics_info(f'checking {session}')
+            potential_job_request = get_last_line(session)
+            if potential_job_request.startswith('#'):
+                optics_debug('job request scanner encountered comment line - skipping this pass...')
+                continue
+            machine,command = parse_job_request(potential_job_request)
+            optics_info(f' machine: {machine} command: {command}')
+            types_to_skip = self.optics_spec.types_to_skip
+            if command == JOB_REQUEST:
+                optics_info(f'job request from {machine} for {self.optics_spec.name}')
                 if self.test_register.has_more_scenes_to_run(self.scene_path_list, types_to_skip):
-                    optics_info(f'job request from {machine} for {self.optics_spec.name}')
+                    optics_info(f'HAS MORE SCENES TO RUN')
                     next_scene_path = self.test_register.assign_next_scene(self.scene_path_list, types_to_skip)
                     optics_info(f'assigned {next_scene_path}')
                     add_last_line(session, get_register_control_message(JOB_ASSIGN, next_scene_path))
