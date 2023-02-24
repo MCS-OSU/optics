5c23225570045446b07397ce57c66abbb3f85223
Mazen Alotaibi <alotaima@oregonstat.edu>
2023-02-24 20:22:21 +0000

    Fixed the time issue for duration and idle_time values for OpicsSession class

--- core/optics_session.py
+++ core/optics_session.py
@@ -6,6 +6,7 @@ import time
 
 SESSION_STATE_EMPTY_FILE    = 'empty_file'
 SESSION_STATE_LACKS_REQUEST = 'lacks_request'
+SESSION_STATE_REQUEST_UNANSWERED = 'lacks_assigment'
 
 class OpticsSession():
     def __init__(self, lines):
@@ -16,6 +17,9 @@ class OpticsSession():
         elif len(lines) == 1:
             self.healthy = False
             self.state = SESSION_STATE_LACKS_REQUEST
+        elif len(lines) == 2:
+            self.healthy = False
+            self.state = SESSION_STATE_REQUEST_UNANSWERED
         else:
             self.healthy = True
             header = lines[0]
@@ -35,10 +39,10 @@ class OpticsSession():
                 self.last_activity = self.start_time
             else:
                 self.end_time = lines[-1].split(';')[0]
-                self.duration = '{0:.2f}'.format((int(self.end_time) - int(self.start_time))/60.0)
+                self.duration = int(float(int(self.end_time) - int(self.start_time))/60.0) # in minutes
                 self.last_activity = lines[-1].split(';')[0]
             current_time = int(time.time())
-            self.idle_time = int((current_time - int(self.last_activity))/60.0)
+            self.idle_time = int(float(current_time - int(self.last_activity))/60.0) # in minutes
             self.session_killed_message = ''
             if SESSION_KILLED in lines[-1]:
                 self.session_killed_message = SESSION_KILLED
@@ -49,23 +53,27 @@ class OpticsSession():
     def summary(self):
         print(f'    {self.machine}  {self.job_count} scenes   {self.duration} mins  {self.idle_time} mins idle    {self.session_killed_message}')
 
-    def convert_to_time_format(self, time_in_seconds):
-        days = time_in_seconds // (24 * 3600)
-        hours = (time_in_seconds // 3600) % 24
-        minutes = (time_in_seconds // 60) % 60
-        seconds = time_in_seconds % 60
+    def convert_to_time_format(self, time_in_minutes):
         
+        n  = int(time_in_minutes)
+        # print(f'N:{n}')
+        days = int(n // 1440)
+        # print(f'Days:{days}')
+        n = n % (1440)
+        hours = int(n // 60)
+        # print(f'Hours:{hours}')
+        n %= 60
+        minutes = n
+        # print(f'Minutes:{minutes}\n')
+       
         time_format = ''
         if days > 0:
             time_format += f'{days}d '
-
+        
         # Add leading zero to hours 
         time_format += f'{hours:02d}h '
 
         # Add leading zero to minutes 
         time_format += f'{minutes:02d}m '
-
-        if seconds > 0:
-            time_format += f'{seconds}s '
 
         return time_format
