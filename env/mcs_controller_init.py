cebd86c46e05a26178348d83af8897c6caf86b35
Jed Irvine <jedirv@gmail.com>
2023-03-16 15:26:47 -0700

    timeout set as minutes; can find file using CONDA_PREFIX

--- env/mcs_controller_init.py
+++ env/mcs_controller_init.py
@@ -6,39 +6,65 @@ class McsControllerInit():
     def __init__(self, known_paths):
         self.known_paths = known_paths
         
-    def patch_timeout(self, num_seconds):
+    def patch_timeout_for_path(self, path, num_minutes):
+        if os.path.exists(path):
+            print(f'mcs init file located at : {path}')
+            print(f'{num_minutes} minutes will be converted to {int(num_minutes)*60} seconds for the TIME_LIMIT_SECONDS value ')
+            f = open(path,'r')
+            lines = f.readlines()
+            f.close()
+            mcs_init_tmp_path = 'tmp_patch_copy'
+            f = open(mcs_init_tmp_path,'w')
+            for line in lines:
+                if line.startswith('TIME_LIMIT_SECONDS'):
+                    print(f'...initally set as:')
+                    print(line)
+                    f.write(f'TIME_LIMIT_SECONDS = {int(num_minutes)*60}\n')
+                else:
+                    f.write(line)
+            f.close()
+
+            os.system(f'rm {path}')
+            os.system(f'mv {mcs_init_tmp_path} {path}')
+            print(f'...now set to: ')
+            os.system(f'cat {path} | grep "TIME_LIMIT_SECONDS ="')
+            print('')
+            return True
+        return False
+
+    def use_conda_env_prefix_to_find_file(self):
+        if not 'CONDA_PREFIX' in os.environ:
+            return False
+        root = os.environ['CONDA_PREFIX']
+        tmp_file = 'junk.txt'
+        os.system(f'find {root} | grep machine_common_sense/__init > {tmp_file}')
+        f = open(tmp_file, 'r')
+        lines = f.readlines()
+        f.close()
+        os.system(f'rm {tmp_file}')
+        for line in lines:
+            if line.startswith(root):
+                return line.rstrip()
+        return 'could not find init file for mcs'
+
+
+    def patch_timeout(self, num_minutes):
         print('')
         print('attempting to patch installed package machine_common_sense/__init__.py TIME_LIMIT_SECONDS value...')
         print('')
         patched = False
         for path in self.known_paths:
-            if os.path.exists(path):
-                print(f'mcs init file located at : {path}')
-                f = open(path,'r')
-                lines = f.readlines()
-                f.close()
-                mcs_init_tmp_path = 'tmp_patch_copy'
-                f = open(mcs_init_tmp_path,'w')
-                for line in lines:
-                    if line.startswith('TIME_LIMIT_SECONDS'):
-                        print(f'...initally set as:')
-                        print(line)
-                        f.write(f'TIME_LIMIT_SECONDS = {num_seconds}\n')
-                    else:
-                        f.write(line)
-                f.close()
-
-                os.system(f'rm {path}')
-                os.system(f'mv {mcs_init_tmp_path} {path}')
-                print(f'...now set to: ')
-                os.system(f'cat {path} | grep "TIME_LIMIT_SECONDS ="')
-                print('')
+            if self.patch_timeout_for_path(path, num_minutes):
                 patched = True
+
         if not patched:
-            print('')
-            print('ERROR - unable to patch machine_common_sense/__init__.py with new timeout - file not in expected location - did you forget to activate the conda environment?')
-            print('')
-                
+            error_string = '\nERROR - unable to patch machine_common_sense/__init__.py with new timeout - file not in expected locations - did you forget to activate the conda environment?\n'
+            path = self.use_conda_env_prefix_to_find_file()
+            if os.path.exists(path):
+                if not self.patch_timeout_for_path(path, num_minutes):
+                    print(error_string)
+            else:
+                print(error_string)
                 
                 
 class McsControllerInitPvoe(McsControllerInit):
--- scripts/patch_mcs_controller_timeout.py
+++ scripts/patch_mcs_controller_timeout.py
@@ -6,7 +6,7 @@ from env.mcs_controller_init import McsControllerInitAvoe
 from env.mcs_controller_init import McsControllerInitInter
 
 def usage():
-    print('usage:  python patch_controller_timeout.py pvoe|avoe|inter num_seconds')
+    print('usage:  python patch_controller_timeout.py pvoe|avoe|inter num_minutes')
 if __name__ == '__main__':
    
     if len(sys.argv) < 3:
@@ -19,22 +19,22 @@ if __name__ == '__main__':
         usage()
         sys.exit()
         
-    num_seconds = sys.argv[2]
+    num_minutes = sys.argv[2]
     try:
-        int_seconds = int(num_seconds)
+        int_minutes = int(num_minutes)
     except Exception as err:
         print(err)
-        print(f'...given num_seconds arg is not an int: {num_seconds}')
+        print(f'...given num_minutes arg is not an int: {num_minutes}')
     
     if proj == 'avoe':
         mcs_init = McsControllerInitAvoe()
-        mcs_init.patch_timeout(num_seconds)
+        mcs_init.patch_timeout(num_minutes)
     elif proj == 'inter':
         mcs_init = McsControllerInitInter()
-        mcs_init.patch_timeout(num_seconds)
+        mcs_init.patch_timeout(num_minutes)
     elif proj == 'pvoe':
         mcs_init = McsControllerInitPvoe()
-        mcs_init.patch_timeout(num_seconds)
+        mcs_init.patch_timeout(num_minutes)
         
 
         
