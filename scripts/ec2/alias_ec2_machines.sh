927c833b00b3f0038647277cbd7cef8cd485b4d5
Mazen Alotaibi <alotaima@oregonstat.edu>
2023-03-30 18:59:41 +0000

    add comments that show the command line ip addresses for each machine

--- scripts/ec2/alias_ec2_machines.sh
+++ scripts/ec2/alias_ec2_machines.sh
@@ -10,3 +10,7 @@ export EC2B_URL=ubuntu@3.20.113.119
 export EC2C_URL=ubuntu@3.221.218.227
 export EC2D_URL=ubuntu@18.207.99.209
 
+echo "ec2a has 172-31-32-89 on command line"
+echo "ec2b has 172-31-12-56 on command line"
+echo "ec2c has 172-31-72-254 on command line"
+echo "ec2d has 172-31-10-162 on command line"
