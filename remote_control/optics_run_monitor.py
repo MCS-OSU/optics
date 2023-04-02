ee12268a5c7e36794ccc6ecbf846e5e308115c70
Jed Irvine <jedirv@gmail.com>
2023-04-02 15:00:59 -0700

    enable print statedments to debug the monitor on ec2 machines

--- remote_control/optics_run_monitor.py
+++ remote_control/optics_run_monitor.py
@@ -38,20 +38,20 @@ class OpticsRunMonitor():
         root_dir = os.path.join(home_dir, 'test__' + run_name)
         dir = os.path.join(root_dir, 'scripts', 'scenes_being_run')
         files = os.listdir(dir)
-        #print(f'process count: {len(optics_processes)}, marker file count: {len(files)}')
+        print(f'process count: {len(optics_processes)}, marker file count: {len(files)}')
         if len(files) == 0 and len(optics_processes) == 0:
             return True
-        #print('NO  evidence of run complete')
+        print('NO  evidence of run complete')
         return False
 
     def is_evidence_of_crash(self, run_name):
         optics_processes = get_running_optics_processes(run_name)
-        #print(f' matching process count: {len(optics_processes)}')
+        print(f' matching process count: {len(optics_processes)}')
         running_scene_marker_present = not self.is_scenes_being_run_empty(run_name)
-        #print(f'running_scene_marker_present: {running_scene_marker_present}')
+        print(f'running_scene_marker_present: {running_scene_marker_present}')
         if running_scene_marker_present and len(optics_processes) == 0:
             return True
-        #print('NO  evidence of crash')
+        print('NO  evidence of crash')
         return False
 
     def is_scenes_being_run_empty(self, run_name):
@@ -69,7 +69,7 @@ class OpticsRunMonitor():
         cur_running_scene_dir = os.path.join(root_dir, 'scripts', 'scenes_being_run')
         files = os.listdir(cur_running_scene_dir)
         if len(files) == 0:
-            #print('no file in scenes_being_run dir, so no evidence of scene running too long')
+            print('no file in scenes_being_run dir, so no evidence of scene running too long')
             return False
         # will always be at most one file, but just in case
         for file in files:
@@ -78,11 +78,11 @@ class OpticsRunMonitor():
             mtime = os.path.getmtime(file_path)
             max_run_time = max_run_time_for_type[scene_type] * 60
             duration = time.time() - mtime
-            #print(f'duration: {duration}     max_run_time: {max_run_time}')
+            print(f'duration: {duration}     max_run_time: {max_run_time}')
             if duration > max_run_time:
                 log_event(f'\n         scene {file} exceeded allowed time of {max_run_time_for_type[scene_type]}')
                 return True
-        #print('NO evidence of scene running too long')
+        print('NO evidence of scene running too long')
         return False
 
 
