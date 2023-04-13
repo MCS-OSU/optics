c00f32a80acd9480788f6d95980753e28d5389fe
Mazen Alotaibi <alotaima@oregonstat.edu>
2023-04-13 20:27:53 +0000

    script to init messaging area on ec2b

--- remote_control/constants.py
+++ remote_control/constants.py
@@ -5,8 +5,8 @@ CLIENT_POLLING_DELAY = 2
 SERVER_POLLING_DELAY = 2
 
 REMOTE_ROOT =  '/home/ubuntu/optics_remote_control'
-TO_USER_REMOTE_DIR = 'messages_to_user'
-FROM_USER_REMOTE_DIR = 'messages_from_user'
+TO_USER_REMOTE_DIR = 'messages_to_runner'
+FROM_USER_REMOTE_DIR = 'messages_from_runner'
 
 # roughly 3x average
 max_run_time_for_type = {
